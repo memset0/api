a46807594aeb158b5d1ed0deb384e3a2331ffffd
memset0 <34177126+memset0@users.noreply.github.com>
2018-05-04 23:02:03 +0800

    Update bing.php

--- bing.php
+++ bing.php
@@ -2,7 +2,7 @@
 // 获取每日必应壁纸
 // DEMO地址: api0.cf/bing.php
 // 直接返回图片因此可以直接当作图片来引用
-// 如在HTML代码中调用 <img src="bing.php" />
+// 如在HTML代码中调用 <img src="http://api0.cf/bing.php" />
 $str=file_get_contents('https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1');
 $imgurl='https://cn.bing.com'.explode('"', explode('url":"', $str)[1])[0];
 if($imgurl){
